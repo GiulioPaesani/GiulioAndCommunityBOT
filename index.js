94f56fea5125e872b235ff2674c90e07997107a2
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-02-21 09:51:36 +0100

    Debug commands channels

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ client.on("message", (message) => {
             comandi: ["!code", "!serverinfo", "!serverstas", "!userinfo", "!userstats", "!roleinfo", "!avatar", "!youtube", "!lastvideo", "!github"],
             id: "802184359120863272",
             canaliPermessi: ["801019779480944660"]
-        },
+        }/*,
         giulioAndCommunityBot: {
             comandi: ["!cuser", "!cserver"],
             id: "802184359120863272",
             canaliPermessi: ["801019779480944660", "793781899796938802"]
-        },
+        }*/,
         mee6: {
             comandi: ["!ban", "!tempban", "!clear", "!nfractions", "!kick", "!mute", "!tempmute", "!slowmode", "!unban", "!unmute", "!warm"],
             id: "159985870458322944",
