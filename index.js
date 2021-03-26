0bc1bce6a960e3ff7b24853a056685503a81c7b5
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-03-26 16:05:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ var ruoliMod = [
 ]
 
 client.on("message", (message) => {
+    if (message.guild.id != "793776260350083113") return
+
     message.content = message.content.trim().toLowerCase();
 
     if (message.channel == "804688929109966848" && !message.content.startsWith("!clear")) return
