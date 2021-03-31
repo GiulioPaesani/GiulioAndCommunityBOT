5262c944ad482541ef93b244deef4f124a7b20c0
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-03-31 16:15:37 +0200

    Add !config

--- index.js
+++ index.js
@@ -201,6 +201,11 @@ client.on("message", (message) => {
             "-!tban": [],
             "-!tkick": [],
             "-!tunban": [],
+
+            "!config": ["801019779480944660"],
+            "!notifiche": ["801019779480944660"],
+            "!notification": ["801019779480944660"],
+            "!notifications": ["801019779480944660"],
         }
 
         var nomeComando;
@@ -2441,6 +2446,149 @@ client.on("message", (message) => {
 
                 message.channel.send(embed)
             }
+        }
+
+        //NOTIFICATION
+        if (message.content == "!config" || message.content == "!notifiche" || message.content == "!notification" || message.content == "!notifications") {
+            var embed = new Discord.MessageEmbed()
+                .setTitle("Notification - " + message.member.user.tag)
+                .setDescription("Impostare le notifiche da ricevere all'interno del server")
+                .setThumbnail("https://i.postimg.cc/cLQ8kP4d/bell.png")
+                .addField(message.member.roles.cache.has("815649916574629941") ? "📋 Announcements - :green_circle: ON" : "📋 Announcements - :red_circle: OFF", "Notifiche sugli annuncio o notizie importanti sul canale/server")
+                .addField(message.member.roles.cache.has("815649625591382077") ? "📰 News - :green_circle: ON" : "📰 News - :red_circle: OFF", "Notifiche su annunci un po' meno importanti")
+                .addField(message.member.roles.cache.has("826784690487951370") ? "📝 Changelog - :green_circle: ON" : "📝 Changelog - :red_circle: OFF", "Notifiche sulle nuove aggiunte e bug risolti dei bot ufficiali")
+                .addField(message.member.roles.cache.has("826776846749925376") ? "🟣 Twitch - :green_circle: ON" : "🟣 Twitch - :red_circle: OFF", "Notifiche sulle dirette, cosa si farà in live, o eventi")
+
+            message.channel.send(embed)
+                .then((msg) => {
+                    msg.react("📋")
+                    msg.react("📰")
+                    msg.react("📝")
+                    msg.react("🟣")
+
+                    // Filters
+                    const reactAnnouncements = (reaction, user) => reaction.emoji.name === '📋'
+                    const reactNews = (reaction, user) => reaction.emoji.name === '📰'
+                    const reactTwitch = (reaction, user) => reaction.emoji.name === '🟣'
+                    const reactChangelog = (reaction, user) => reaction.emoji.name === '📝'
+
+                    const paginaAnnouncements = msg.createReactionCollector(reactAnnouncements)
+                    const paginaNews = msg.createReactionCollector(reactNews)
+                    const paginaTwitch = msg.createReactionCollector(reactTwitch)
+                    const paginaChangelog = msg.createReactionCollector(reactChangelog)
+
+                    paginaAnnouncements.on('collect', (r, u) => {
+                        if (u.bot)
+                            return
+                        r.users.remove(r.users.cache.filter(u => u.bot == false).first())
+                        if (u.id == message.author.id) {
+
+                            var embed = new Discord.MessageEmbed()
+                                .setTitle("Notification - " + message.member.user.tag)
+                                .setDescription("Impostare le notifiche da ricevere all'interno del server")
+                                .setThumbnail("https://i.postimg.cc/cLQ8kP4d/bell.png")
+
+                            if (!message.member.roles.cache.has("815649916574629941")) {
+                                message.member.roles.add("815649916574629941")
+                                embed
+                                    .addField("📋 Announcements - :green_circle: ON", "Notifiche sugli annuncio o notizie importanti sul canale/server")
+                            }
+                            else {
+                                message.member.roles.remove("815649916574629941")
+                                embed
+                                    .addField("📋 Announcements - :red_circle: OFF", "Notifiche sugli annuncio o notizie importanti sul canale/server")
+                            }
+                            embed
+                                .addField(message.member.roles.cache.has("815649625591382077") ? "📰 News - :green_circle: ON" : "📰 News - :red_circle: OFF", "Notifiche su annunci un po' meno importanti")
+                                .addField(message.member.roles.cache.has("826784690487951370") ? "📝 Changelog - :green_circle: ON" : "📝 Changelog - :red_circle: OFF", "Notifiche sulle nuove aggiunte e bug risolti dei bot ufficiali")
+                                .addField(message.member.roles.cache.has("826776846749925376") ? "🟣 Twitch - :green_circle: ON" : "🟣 Twitch - :red_circle: OFF", "Notifiche sulle dirette, cosa si farà in live, o eventi")
+                            msg.edit(embed)
+                        }
+                    })
+                    paginaNews.on('collect', (r, u) => {
+                        if (u.bot)
+                            return
+                        r.users.remove(r.users.cache.filter(u => u.bot == false).first())
+
+                        if (u.id == message.author.id) {
+                            var embed = new Discord.MessageEmbed()
+                                .setTitle("Notification - " + message.member.user.tag)
+                                .setDescription("Impostare le notifiche da ricevere all'interno del server")
+                                .setThumbnail("https://i.postimg.cc/cLQ8kP4d/bell.png")
+                                .addField(message.member.roles.cache.has("815649916574629941") ? "📋 Announcements - :green_circle: ON" : "📋 Announcements - :red_circle: OFF", "Notifiche sugli annuncio o notizie importanti sul canale/server")
+
+                            if (!message.member.roles.cache.has("815649625591382077")) {
+                                message.member.roles.add("815649625591382077")
+                                embed
+                                    .addField("📰 News - :green_circle: ON", "Notifiche su annunci un po' meno importanti")
+                            }
+                            else {
+                                message.member.roles.remove("815649625591382077")
+                                embed
+                                    .addField("📰 News - :red_circle: OFF", "Notifiche su annunci un po' meno importanti")
+                            }
+                            embed
+                                .addField(message.member.roles.cache.has("826784690487951370") ? "📝 Changelog - :green_circle: ON" : "📝 Changelog - :red_circle: OFF", "Notifiche sulle nuove aggiunte e bug risolti dei bot ufficiali")
+                                .addField(message.member.roles.cache.has("826776846749925376") ? "🟣 Twitch - :green_circle: ON" : "🟣 Twitch - :red_circle: OFF", "Notifiche sulle dirette, cosa si farà in live, o eventi")
+                            msg.edit(embed)
+                        }
+                    })
+                    paginaTwitch.on('collect', (r, u) => {
+                        if (u.bot)
+                            return
+                        r.users.remove(r.users.cache.filter(u => u.bot == false).first())
+
+                        if (u.id == message.author.id) {
+                            var embed = new Discord.MessageEmbed()
+                                .setTitle("Notification - " + message.member.user.tag)
+                                .setDescription("Impostare le notifiche da ricevere all'interno del server")
+                                .setThumbnail("https://i.postimg.cc/cLQ8kP4d/bell.png")
+                                .addField(message.member.roles.cache.has("815649916574629941") ? "📋 Announcements - :green_circle: ON" : "📋 Announcements - :red_circle: OFF", "Notifiche sugli annuncio o notizie importanti sul canale/server")
+                                .addField(message.member.roles.cache.has("815649625591382077") ? "📰 News - :green_circle: ON" : "📰 News - :red_circle: OFF", "Notifiche su annunci un po' meno importanti")
+                                .addField(message.member.roles.cache.has("826784690487951370") ? "📝 Changelog - :green_circle: ON" : "📝 Changelog - :red_circle: OFF", "Notifiche sulle nuove aggiunte e bug risolti dei bot ufficiali")
+
+                            if (!message.member.roles.cache.has("826776846749925376")) {
+                                message.member.roles.add("826776846749925376")
+                                embed
+                                    .addField("🟣 Twitch - :green_circle: ON", "Notifiche sulle dirette, cosa si farà in live, o eventi")
+                            }
+                            else {
+                                message.member.roles.remove("826776846749925376")
+                                embed
+                                    .addField("🟣 Twitch - :red_circle: OFF", "Notifiche sulle dirette, cosa si farà in live, o eventi")
+                            }
+                            msg.edit(embed)
+                        }
+                    })
+                    paginaChangelog.on('collect', (r, u) => {
+                        if (u.bot)
+                            return
+                        r.users.remove(r.users.cache.filter(u => u.bot == false).first())
+
+                        if (u.id == message.author.id) {
+                            var embed = new Discord.MessageEmbed()
+                                .setTitle("Notification - " + message.member.user.tag)
+                                .setDescription("Impostare le notifiche da ricevere all'interno del server")
+                                .setThumbnail("https://i.postimg.cc/cLQ8kP4d/bell.png")
+                                .addField(message.member.roles.cache.has("815649916574629941") ? "📋 Announcements - :green_circle: ON" : "📋 Announcements - :red_circle: OFF", "Notifiche sugli annuncio o notizie importanti sul canale/server")
+                                .addField(message.member.roles.cache.has("815649625591382077") ? "📰 News - :green_circle: ON" : "📰 News - :red_circle: OFF", "Notifiche su annunci un po' meno importanti")
+                            if (!message.member.roles.cache.has("826784690487951370")) {
+                                message.member.roles.add("826784690487951370")
+                                embed
+                                    .addField("📝 Changelog - :green_circle: ON", "Notifiche sulle nuove aggiunte e bug risolti dei bot ufficiali")
+                            }
+                            else {
+                                message.member.roles.remove("826784690487951370")
+                                embed
+                                    .addField("📝 Changelog - :red_circle: OFF", "Notifiche sulle nuove aggiunte e bug risolti dei bot ufficiali")
+                            }
+                            embed
+                                .addField(message.member.roles.cache.has("826776846749925376") ? "🟣 Twitch - :green_circle: ON" : "🟣 Twitch - :red_circle: OFF", "Notifiche sulle dirette, cosa si farà in live, o eventi")
+                            msg.edit(embed)
+                        }
+                    })
+                })
+
         }
 
     })
