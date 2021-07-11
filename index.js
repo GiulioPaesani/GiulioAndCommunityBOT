374908b6529ed5e6a8aff97a76914290d0a0c2ba
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-07-11 19:07:05 +0200

    Update Forzaitalia

--- index.js
+++ index.js
@@ -3699,7 +3699,7 @@ https://www.youtube.com/watch?v=${response.items[0].videoId}
     }
 }, 1000)
 
-var secondi = 3;
+var secondi = 5;
 setInterval(function () {
     if (secondi > 0)
         secondi--;
@@ -3707,13 +3707,13 @@ setInterval(function () {
 client.on("message", async message => {
     if (message.content == "!forza" || message.content == "!forzaitalia" || message.content == "!forzaitalia" || message.content == "!euro2020") {
         if (secondi == 0) {
-            secondi = 3
+            secondi = 5
             await message.channel.send("FORZA ITALIA! :flag_it: :flag_it: :flag_it: ")
             await message.channel.send("FORZA ITALIA! :flag_it: :flag_it: :flag_it: ")
             await message.channel.send("FORZA ITALIA! :flag_it: :flag_it: :flag_it: ")
         }
         else {
-            message.channel.send("Questo comando ha un cooldown di 3 secondi")
+            message.channel.send("Questo comando ha un cooldown di **5 secondi**")
                 .then(msg => {
                     msg.delete({ timeout: 10000 })
                     message.delete({ timeout: 10000 })
