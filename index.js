e674e2c8f9988d0741f4de9d9da20133232fcce3
GiulioPaesani <giuliopaesani@gmail.com>
2021-07-31 16:07:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -128,18 +128,6 @@ client.on("message", message => {
     catch (err) {
         codeError(err)
     }
-
-    database.collection("userstats").find().toArray(function (err, result) {
-        if (err) return codeError(err);
-        var userstatsList = result;
-
-        var userstats = userstatsList.find(x => x.id == message.author.id)
-        if (!userstats) return
-
-        userstats.statistics.commands++;
-
-        database.collection("userstats").updateOne({ id: userstats.id }, { $set: userstats });
-    })
 })
 
 
