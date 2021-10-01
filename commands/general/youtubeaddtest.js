7dec46be8494634dff6b3d3d6f56224d314e6e5e
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-10-01 18:57:35 +0200

    Test Youtube Poster

--- commands/general/youtubeaddtest.js
+++ commands/general/youtubeaddtest.js
@@ -6,34 +6,49 @@ module.exports = {
     onlyStaff: true,
     channelsGranted: [],
     async execute(message, args, client) {
-        let ChannelLink = args[0];
-        let DiscordChannel =
-            message.mentions.channels
-                .filter((c) => c.guild.id == message.guild.id)
-                .first() || message.guild.channels.cache.get(args[1]);
-        let DiscordUser =
-            message.mentions.members
-                .filter((m) => m.guild.id == message.guild.id)
-                .first()?.user || message.guild.members.cache.get(args[2])?.user;
-        let Notification =
-            args.slice(3).join(" ") || client.YTP.options.defaults.Notification;
-        let preventDuplicates = true;
-        if (!ChannelLink || !DiscordChannel || !DiscordUser)
-            return message.channel.send({
-                embed: new Discord.MessageEmbed()
-                    .setColor("RED")
-                    .setDescription(
-                        `:x: Usage: \`${prefix}set <LINK> <CHANNEL> <USER> [TEXT...]\`\n\n**Replacements:**\n` +
-                        toreplace_format
-                    ),
-            });
-        //set a Channel
-        client.YTP.setChannel(
+
+        await client.YTP.deleteChannel(message.guild.id, "https://www.youtube.com/channel/UCK6QwAdGWOWN9AT1_UQFGtA")
+
+        var canale = await client.channels.cache.get("869975176895418438")
+
+        await client.YTP.setChannel(
             "https://www.youtube.com/channel/UCK6QwAdGWOWN9AT1_UQFGtA",
-            DiscordChannel,
-            DiscordUser,
+            canale,
+            message.member,
             `-------------:computer: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :computer:-------------
 Ehy ragazzi, è appena uscito un nuovo video su **GiulioAndCode**
+Andate subito a vedere "**{videotitle}**"
+
+{videourl}
+<@&857544584691318814>`,
+            (preventDuplicates = true)
+        )
+            .then((ch) => {
+                console.log(ch)//See the Responses: https://github.com/tovade/discord-youtube/wiki/Responses
+                //send the information
+                message.channel
+                    .send({
+                        embed: new Discord.MessageEmbed()
+                            .setColor("GREEN")
+                            .setDescription(
+                                `I will now post Notifications for ${ch.YTchannel} (<@${ch.DiscordUser}>) in <#${ch.DiscordChannel}>\n\nThe Message:\n${ch.message}`
+                            ),
+                    })
+                    .then((msg) => msg.react("👍"));
+            })
+            .catch((e) => {
+                console.log(e);
+                message.channel.send(`${e.message ? e.message : e}`, {
+                    code: "js",
+                });
+            });
+
+        await client.YTP.setChannel(
+            "https://www.youtube.com/channel/UCvIafNR8ZvZyE5jVGVqgVfA",
+            canale,
+            message.member,
+            `-------------:v: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :v:-------------
+Ehy ragazzi, è appena uscito un nuovo video su **Giulio**
 Andate subito a vedere "**{videotitle}**"
 
 {videourl}
@@ -59,5 +74,8 @@ Andate subito a vedere "**{videotitle}**"
                     code: "js",
                 });
             });
+
+
+
     },
 };
