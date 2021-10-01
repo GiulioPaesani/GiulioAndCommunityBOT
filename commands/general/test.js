f4ab2a76720f4f364cd81b60a4e2b9332d89a092
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-10-01 19:41:27 +0200

    Test Youtube Poster

--- commands/general/test.js
+++ commands/general/test.js
@@ -16,5 +16,19 @@ module.exports = {
             .addField(":turtle: Ping", "```" + `${client.ws.ping}ms` + "```", true)
             .addField(":floppy_disk: Ram", "```" + `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB` + "```", true)
         message.channel.send(embed)
+
+        client.YTP.getAllChannels(message.guild.id)
+            .then(chs => {
+                //console.log(ch) See the Responses: https://github.com/Tomato6966/discord-yt-poster/wiki/Responses
+                //send information, you could do a chs.map(ch=>ch.YTchannel)...
+                message.channel.send({
+                    embed: new Discord.MessageEmbed().setColor("GREEN").setDescription(`There are ${chs.length} Channels Setupped!`)
+                }).then(msg => msg.react("👍"))
+            }).catch(e => {
+                console.log(e);
+                message.channel.send(`${e.message ? e.message : e}`, {
+                    code: "js"
+                })
+            })
     },
 };
