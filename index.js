ce19461cf7c21a597e1493c9ea38120df82ab47a
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-10-02 14:51:33 +0200

    Test Youtube Poster

--- index.js
+++ index.js
@@ -164,3 +164,58 @@ process.on("uncaughtException", err => {
 process.on("unhandledRejection", err => {
     codeError(err);
 })
+
+client.on("message", message => {
+    //if in a dm or msg from a bot, return 
+    if (!message.guild || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "set" || cmd === "add" || cmd === "youtube") {
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send({
+            embed: new Discord.MessageEmbed().setColor("RED").setDescription(":x: You are not allowed to execute this Command!")
+        })
+        let ChannelLink = args[0];
+        let DiscordChannel = message.mentions.channels.filter(c => c.guild.id == message.guild.id).first() || message.guild.channels.cache.get(args[1]);
+        let DiscordUser = message.mentions.members.filter(m => m.guild.id == message.guild.id).first()?.user || message.guild.members.cache.get(args[2])?.user;
+        let Notification = args.slice(3).join(" ") || client.YTP.options.defaults.Notification;
+        let preventDuplicates = true;
+        if (!ChannelLink || !DiscordChannel || !DiscordUser) return message.channel.send({
+            embed: new Discord.MessageEmbed().setColor("RED").setDescription(`:x: Usage: \`${prefix}set <LINK> <CHANNEL> <USER> [TEXT...]\`\n\n**Replacements:**\n` + toreplace_format)
+        })
+        //set a Channel
+        client.YTP.setChannel(ChannelLink, DiscordChannel, DiscordUser, Notification, preventDuplicates = true)
+            .then(ch => {
+                //console.log(ch) See the Responses: https://github.com/Tomato6966/discord-yt-poster/wiki/Responses
+                //send the information
+                message.channel.send({
+                    embed: new Discord.MessageEmbed().setColor("GREEN").setDescription(`I will now post Notifications for ${ch.YTchannel} (<@${ch.DiscordUser}>) in <#${ch.DiscordChannel}>\n\nThe Message:\n${ch.message}`)
+                }).then(msg => msg.react("👍"))
+            }).catch(e => {
+                console.log(e);
+                message.channel.send(`${e.message ? e.message : e}`, {
+                    code: "js"
+                })
+            })
+    }
+
+
+    if (cmd === "getallchannels") {
+        //get all channels
+        client.YTP.getAllChannels(message.guild.id)
+            .then(chs => {
+                //console.log(ch) See the Responses: https://github.com/Tomato6966/discord-yt-poster/wiki/Responses
+                //send information, you could do a chs.map(ch=>ch.YTchannel)...
+                message.channel.send({
+                    embed: new Discord.MessageEmbed().setColor("GREEN").setDescription(`There are ${chs.length} Channels Setupped!`)
+                }).then(msg => msg.react("👍"))
+            }).catch(e => {
+                console.log(e);
+                message.channel.send(`${e.message ? e.message : e}`, {
+                    code: "js"
+                })
+            })
+    }
+
+})
