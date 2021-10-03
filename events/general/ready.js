25f11ce0ce453e62a98b7645f85d721a86a5f257
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-10-03 20:27:16 +0200

    Test Youtube Poster

--- events/general/ready.js
+++ events/general/ready.js
@@ -37,19 +37,26 @@ module.exports = {
         };
         client.YTP = new YoutubePoster(client, options)
 
-        // await client.YTP.setChannel("https://www.youtube.com/channel/UCK6QwAdGWOWN9AT1_UQFGtA", client.channels.cache.get("869975176895418438"), client.users.cache.get("793768313934577664"), `-------------:computer: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :computer:-------------
-        // Ehy ragazzi, è appena uscito un nuovo video su **GiulioAndCode**
-        // Andate subito a vedere "**{videotitle}**"
+        await client.YTP.setChannel("https://www.youtube.com/channel/UCK6QwAdGWOWN9AT1_UQFGtA", client.channels.cache.get("869975176895418438"), client.users.cache.get("793768313934577664"), `-------------:computer: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :computer:-------------
+Ehy ragazzi, è appena uscito un nuovo video su **GiulioAndCode**
+Andate subito a vedere "**{videotitle}**"
 
-        // {videourl}
-        // <@&857544584691318814>`, preventDuplicates = true)
+{videourl}
+<@&857544584691318814>`, preventDuplicates = true)
 
-        // await client.YTP.setChannel("https://www.youtube.com/channel/UCvIafNR8ZvZyE5jVGVqgVfA", client.channels.cache.get("869975176895418438"), client.users.cache.get("793768313934577664"), `-------------:v: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :v:-------------
-        // Ehy ragazzi, è appena uscito un nuovo video su **Giulio**
-        // Andate subito a vedere "**{videotitle}**"
+        await client.YTP.setChannel("https://www.youtube.com/channel/UCvIafNR8ZvZyE5jVGVqgVfA", client.channels.cache.get("869975176895418438"), client.users.cache.get("793768313934577664"), `-------------:v: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :v:-------------
+Ehy ragazzi, è appena uscito un nuovo video su **Giulio**
+Andate subito a vedere "**{videotitle}**"
 
-        // {videourl}
-        // <@&883062518774370345>`, preventDuplicates = true)
+{videourl}
+<@&883062518774370345>`, preventDuplicates = true)
+
+        await client.YTP.setChannel("https://www.youtube.com/channel/UCkbF1Vs7k5qgtuO74cV3Feg", client.channels.cache.get("869975176895418438"), client.users.cache.get("793768313934577664"), `-------------:v: **𝐍𝐄𝐖 𝐕𝐈𝐃𝐄𝐎** :v:-------------
+Ehy ragazzi, è appena uscito un nuovo video su **TESTING**
+Andate subito a vedere "**{videotitle}**"
+
+{videourl}
+<@&883062518774370345>`, preventDuplicates = true)
 
         var embed = new Discord.MessageEmbed()
             .setTitle("Bot ONLINE")
