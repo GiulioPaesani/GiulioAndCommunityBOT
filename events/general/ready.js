c90b813a48be47b68689d45737b0496db767e719
giuliopaesani04 <57570629+GiulioPaesani@users.noreply.github.com>
2021-11-28 17:34:04 +0100

    Add sendAvvento()

--- events/general/ready.js
+++ events/general/ready.js
@@ -28,6 +28,8 @@ module.exports = {
 
         setInterval(checkUnverifedUser, 1000)
 
+        setInterval(sendAvvento, 1000)
+
         var embed = new Discord.MessageEmbed()
             .setTitle("Bot ONLINE")
             .setColor("#3ebd45")
---
+++ functions/avvento.js
@@ -0,0 +1,73 @@
+global.sendAvvento = function () {
+	var day = new Date().getDate()
+	var month = new Date().getMonth()
+
+	var hours = new Date().getHours()
+	var minutes = new Date().getMinutes()
+	var seconds = new Date().getSeconds()
+
+	hours = 0;
+	minutes = 0;
+	seconds = 1;
+
+	day = 12
+	month = 11
+
+	var channel = await client.channels.cache.get("907340145383047168")
+
+	if (month == 11 && day <= 25) {
+		if (hours == 0 && minutes == 0 && seconds == 1) {
+			await channel.messages.fetch().then(messages => {
+				var array = Array.from(messages.values())
+				for (var i = 0; i < array.length; i++) {
+					await array[i].delete().catch(() => { return })
+				}
+			})
+
+			if (day < 25) {
+				await channel.send(`Si sta avvicinando finalmente il **Natale**, è arrivato il momento dei regali, dei pranzi con i parenti e dei bacini dalla zia
+Per attendere con piacere questo grande giorno, potete partecipare all'**Avvento della community**🎄`)
+
+				await channel.send({ files: [avventoJSON.banner.banner] })
+
+				await channel.send(`\u200b
+🎁 **Ogni giorno** fino al 25 Dicembre vi aspetta una **fantastica ricompensa** da riscattare, per poi ricevere un mitico **super regalo** il giorno di Natale!`)
+
+				var button = new disbut.MessageButton()
+					.setLabel("Riscatta ricompense")
+					.setStyle("red")
+					.setID("riscattaRicompense")
+
+				var row = new disbut.MessageActionRow()
+					.addComponents(button)
+
+				await channel.send(`\u200b
+Riscatta tutte le tue ricompense dell'Avvento con il pulsante **"Riscatta ricompense"** qua sotto`)
+
+				await channel.send({ files: [avventoJSON.banner[day]], components: row })
+			}
+			else {
+				await channel.send(`Il Natale è finalmente **arrivato**, il momento dei regali, dei pranzi con i parenti e dei bacini dalla zia, che bello...
+Riscattate tutti le ricompense nell'**Avvento della community**🎄`)
+
+				await channel.send({ files: [avventoJSON.banner.banner] })
+
+				await channel.send(`\u200b
+🎁 Per **ogni giorno** di Dicembre riscattate una **fantastica ricompensa**, vi aspetta anche un mitico **super regalo**`)
+
+				var button = new MessageButton()
+					.setLabel("Riscatta ricompense")
+					.setStyle("red")
+					.setID("riscattaRicompense")
+
+				var row = new MessageActionRow()
+					.addComponents(button)
+
+				await channel.send(`\u200b
+Riscatta tutte le tue ricompense dell'Avvento con il pulsante **"Riscatta ricompense"** qua sotto`)
+
+				await channel.send({ files: [avventoJSON.banner[25]], components: row })
+			}
+		}
+	}
+}
