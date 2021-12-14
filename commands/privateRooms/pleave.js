c2def6cac5a85fabc20fa08b7e27284e3c40ae29
giuliopaesani04 <57570629+GiulioPaesani@users.noreply.github.com>
2021-12-14 17:55:22 +0100

    Add !pleave

---
+++ commands/privateRooms/pleave.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: "pleave",
+    aliases: ["pesci"],
+    onlyStaff: false,
+    channelsGranted: [],
+    async execute(message, args, client) {
+        var privaterooms = serverstats.privateRooms
+
+        if (!privaterooms.find(x => x?.text == message.channel.id)) {
+            warning(message, "Non sei in una stanza", "Per porter uscire da una stanza, scrivi il comando in quella stanza privata")
+            return
+        }
+
+        var room = privaterooms.find(x => x?.text == message.channel.id)
+        if (!room.text) return
+
+        if (room.owner == message.author.id) {
+            warning(message, "Non nella tua stanza", "Non puoi uscire dalla tua stessa stanza")
+            return
+        }
+
+        if (utenteMod(message.author)) {
+            warning(message, "Non puoi uscire", "Putroppo sei un amministratore e non hai possibilità di uscire da questa stanza")
+            return
+        }
+
+        correct(message, "Ti sei rimosso", `${message.author.toString()} si è rimosso da questa stanza`)
+
+        setTimeout(() => {
+            message.channel.updateOverwrite(message.member, {
+                VIEW_CHANNEL: false
+            })
+        }, 2000)
+
+    },
+};
