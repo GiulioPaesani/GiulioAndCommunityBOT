cc30ea889b55a5c5393b0735661c19bb8779b44b
giuliopaesani04 <57570629+GiulioPaesani@users.noreply.github.com>
2021-12-20 18:54:48 +0100

    Update !critica

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ client.on("message", message => {
     if (message.channel?.id == "922542187667918948" && !message.content.startsWith("!critica") && !message.content.startsWith("!critique")) {
         if (message.author.bot) return
 
-        let testo = args.join(" ");
+        let testo = message.content.slice("!".length).trim().split(/ +/);.join(" ");
         if (!testo) {
             return
         }
