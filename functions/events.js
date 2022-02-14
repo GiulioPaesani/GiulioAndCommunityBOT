9a3af680252397d26903f8a2fcf0f431c057bbf5
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2022-02-14 13:50:17 +0100

    Fix bugs

--- functions/events.js
+++ functions/events.js
@@ -3,7 +3,7 @@ setInterval(async function () {
     var data = new Date()
     if (data.getDate() == 14 && data.getMonth() == 1) {
         if (data.getHours() == 0 && data.getMinutes() == 0 && data.getSeconds() == 0) {
-            var server = await client.guilds.cache.get(config.idServer);
+            var server = await client.guilds.cache.get(settings.idServer);
 
             await server.setIcon("https://i.postimg.cc/WtcRmqpY/Profilo-San-Valentino.png");
             await client.user.setAvatar("https://i.postimg.cc/FzL5JVvJ/Profilo-bot-San-Valentino.png");
@@ -12,7 +12,7 @@ setInterval(async function () {
 
     if (data.getDate() == 15 && data.getMonth() == 1) {
         if (data.getHours() == 0 && data.getMinutes() == 0 && data.getSeconds() == 0) {
-            var server = await client.guilds.cache.get(config.idServer);
+            var server = await client.guilds.cache.get(settings.idServer);
 
             await server.setIcon("https://i.postimg.cc/Pr3QZdyC/Profilo-server.png");
             await client.user.setAvatar("https://i.postimg.cc/XvDRzMnq/Profilo-bot.png");
