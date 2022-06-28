132d7c753eef11e9918df1d0072eb3520dcabaa7
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2022-06-28 21:18:33 +0200

    Fix bugs

--- functions/community/poll/checkPollTimeout.js
+++ functions/community/poll/checkPollTimeout.js
@@ -4,7 +4,7 @@ const colors = require("../../../config/general/colors.json")
 const { fetchAllMessages } = require("../../general/fetchAllMessages")
 
 const checkPollTimeout = async (client) => {
-    let channel = client.channels.cache.get(.settings.idCanaliServerpolls)
+    let channel = client.channels.cache.get(settings.idCanaliServer.polls)
 
     fetchAllMessages(channel)
         .then(async messages => {
