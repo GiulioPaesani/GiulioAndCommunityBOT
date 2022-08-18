8ee4077c57a01d94f767ddef2bafb89bf2de4513
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2022-08-18 15:30:40 +0200

    Fix getUser()

--- functions/database/getUser.js
+++ functions/database/getUser.js
@@ -4,24 +4,23 @@ const getUser = async (userId) => {
     try {
         let data = JSON.parse(fs.readFileSync(`./database/users/${userId}.json`, 'utf8'))
 
-        if (data.joinedAt["$numberLong"]) data.joinedAt = data.joinedAt["$numberLong"]
-        if (data.leavedAt["$numberLong"]) data.leavedAt = data.leavedAt["$numberLong"]
+        if (data.joinedAt && data.joinedAt["$numberLong"]) data.joinedAt = data.joinedAt["$numberLong"]
+        if (data.leavedAt && data.leavedAt["$numberLong"]) data.leavedAt = data.leavedAt["$numberLong"]
 
 
-        if (data.counting.timeLastScore["$numberLong"]) data.counting.timeLastScore = data.counting.timeLastScore["$numberLong"]
-        if (data.counting.timeBestScore["$numberLong"]) data.counting.timeBestScore = data.counting.timeBestScore["$numberLong"]
+        if (data.counting.timeLastScore && data.counting.timeLastScore["$numberLong"]) data.counting.timeLastScore = data.counting.timeLastScore["$numberLong"]
+        if (data.counting.timeBestScore && data.counting.timeBestScore["$numberLong"]) data.counting.timeBestScore = data.counting.timeBestScore["$numberLong"]
 
-        if (data.countingplus.timeLastScore["$numberLong"]) data.countingplus.timeLastScore = data.countingplus.timeLastScore["$numberLong"]
+        if (data.countingplus.timeLastScore && data.countingplus.timeLastScore["$numberLong"]) data.countingplus.timeLastScore = data.countingplus.timeLastScore["$numberLong"]
 
-        if (data.countingplus.timeLastScore["$numberLong"]) data.countingplus.timeLastScore = data.countingplus.timeLastScore["$numberLong"]
+        if (data.countingplus.timeLastScore && data.countingplus.timeLastScore["$numberLong"]) data.countingplus.timeLastScore = data.countingplus.timeLastScore["$numberLong"]
 
-        if (data.moderation.since["$numberLong"]) data.moderation.since = data.moderation.since["$numberLong"]
-        if (data.moderation.until["$numberLong"]) data.moderation.until = data.moderation.until["$numberLong"]
-
+        if (data.moderation.since && data.moderation.since["$numberLong"]) data.moderation.since = data.moderation.since["$numberLong"]
+        if (data.moderation.until && data.moderation.until["$numberLong"]) data.moderation.until = data.moderation.until["$numberLong"]
 
         if (data.warns.some(x => x.time["$numberLong"])) {
             for (let index in data.warns) {
-                if (data.warns[index].time["$numberLong"]) data.warns[index].time = data.warns[index].time["$numberLong"]
+                if (data.warns[index].time && data.warns[index].time["$numberLong"]) data.warns[index].time = data.warns[index].time["$numberLong"]
             }
         }
 
