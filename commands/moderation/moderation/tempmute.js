a1c01dc34bb7626a936e1b2f0a9ec4b660847595
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2022-10-28 18:04:03 +0200

    Debug

--- commands/moderation/moderation/tempmute.js
+++ commands/moderation/moderation/tempmute.js
@@ -271,10 +271,13 @@ module.exports = {
             .addField(":hourglass: Time", ms(time, { long: true }))
             .addField(":shield: Moderator", interaction.user.toString())
             .setFooter({ text: "User ID: " + utente.id })
+
         console.log("PRIMA DEL REPLY")
-        interaction.reply({ embeds: [embed] })
+        interaction.reply({ embeds: [embed] }).then(() => console.log("REPLY MANDATO"))
         // let msg = await interaction.reply({ embeds: [embed], fetchReply: true })
         console.log("DOPO IL REPLY")
+        return
+
         embed = new Discord.MessageEmbed()
             .setTitle(":speaker: Tempmute :speaker:")
             .setColor(colors.purple)
